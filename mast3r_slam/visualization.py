ab4c5b2b36e1093d1c1271491d6e3d6d03d26c61
Nikita Demir <nikitde1@gmail.com>
2025-07-17 17:48:14 -0400

    wip

--- mast3r_slam/visualization.py
+++ mast3r_slam/visualization.py
@@ -38,19 +38,24 @@ class Viewer:
 
     def add_keyframe(self, keyframe: Frame):
         t, q = as_SE3(keyframe.T_WC.cpu()).data.split([3, 4], -1)
+        t = t.flatten().numpy()
+        q = q.flatten().numpy()
         self.scene.add_camera_frustum(f"/keyframes/{keyframe.frame_id}", 
                                       fov=55, 
                                       aspect=1.0,
                                       scale=0.1,
-                                      position=t.flatten().numpy(),
-                                      wxyz=q.flatten().numpy())
+                                      position=t,
+                                      wxyz=q)
 
-        if keyframe.X_canon is not None and keyframe.C is not None:
+        if keyframe.X_canon is not None:
             self.scene.add_point_cloud(f"/points/{keyframe.frame_id}", 
                                     points=keyframe.X_canon.cpu().numpy().astype(np.float32) ,
                                     colors=(0, 1, 0),
                                     # colors=keyframe.C.cpu().numpy().astype(np.float32), # TODO: I think this is something else like confidence
-                                    point_size=0.01
+                                    point_size=0.0001,
+                                    point_shape="circle",
+                                    position=t,
+                                    wxyz=q
                                     )
 
 
